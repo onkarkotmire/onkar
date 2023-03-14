3e47d96b168da6a1e2464b979187bbd111ab7787
unknown <onkarkotmire2017@gmail.com>
2023-03-14 17:26:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Evolution from './components/Evolution';
+
+function App() {
+  return (
+    <div className="App">
+      <Evolution/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Evolution.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Evolution.css'
+
+export default function Evolution() {
+  return (
+    <div className='body'>
+      <h1>The Evolution of <br />
+       Airbnb's Frontend</h1>
+      <h3>@spikebrehm</h3>
+      <div className='icon'>
+      <i class="fa-brands fa-airbnb" id='iconsymbol'></i>airbnb
+      </div>
+    </div>
+  )
+}
